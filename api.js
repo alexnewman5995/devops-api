48b5222d9cae1c0c98cb921844ceba52f33a09eb
SiamakShams <SiamakShams@users.noreply.github.com>
2021-05-26 17:51:07 +0100

    Using port 8080

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 const http = require('http'); 
 
-const port = process.env.PORT || 80 
+const port = process.env.PORT || 8080
 
 const server = http.createServer((req, res) => { 
     if (req.method == 'GET') { 
