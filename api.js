0232ccd70439886cc2135be62dfba261ea93d3f8
SiamakShams <siamak.shams@gmail.com>
2021-07-04 21:44:05 +0100

    Changed application api messagege.

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080
 const server = http.createServer((req, res) => { 
     if (req.method == 'GET') { 
         if (req.url === '/') { 
-            res.end(`<h1>Hello World</h1>`) 
+            res.end(`<h1>Hello DevOps World</h1>`) 
         } 
         if (req.url === '/hello') { 
             res.end(`<h1>Go away!</h1>`) 
